fbc1cfc4e92c073bd9d737f7b9329cfae4ef74be
Alex <alexandercastronovo@gmail.com>
2024-12-04 22:52:12 -0500

    adjust scene - bug: everything is gray

--- src/main/java/raytracer/RayTracer.java
+++ src/main/java/raytracer/RayTracer.java
@@ -20,8 +20,8 @@ public class RayTracer {
         textureShader = new TextureShader();
         rayTracerCompute = new RayTracerCompute();
         screenTexture = new ScreenTexture(width, height);
-        objectsBuffer = new ObjectsBuffer(new Vector3f[]{new Vector3f(0, -100.5f, -1), new Vector3f(0, 160, -1), new Vector3f(0, 0, -1)}, new float[]{100f, 110f, 0.5f}, new int[]{0, 1, 0});
-        materialsBuffer = new MaterialsBuffer(new Vector3f[]{new Vector3f(1, 1, 1), new Vector3f(0.5f, 0.5f, 0.5f)}, new Vector3f[]{new Vector3f(0, 0, 0), new Vector3f(1, 1, 1)}, new float[]{0, 0.9f}, new int[]{0, 0}, new float[]{0, 0});
+        objectsBuffer = new ObjectsBuffer(new Vector3f[]{new Vector3f(0, -100.5f, -1), new Vector3f(100, 160, -1), new Vector3f(0, 0, -1)}, new float[]{100f, 110f, 0.5f}, new int[]{0, 1, 0});
+        materialsBuffer = new MaterialsBuffer(new Vector3f[]{new Vector3f(1, 1, 1), new Vector3f(1, 1, 1)}, new Vector3f[]{new Vector3f(0, 0, 0), new Vector3f(0.9f, 0.9f, 0.9f)}, new float[]{0, 0.9f}, new int[]{0, 0}, new float[]{0, 0});
     }
 
     public void run() {
