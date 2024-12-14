497ba878e98a3a3ceeac5ec8ece8aa97abb9b3b0
Alex <alexandercastronovo@gmail.com>
2024-12-14 17:54:55 -0500

    cleanup

--- src/main/java/raytracer/Main.java
+++ src/main/java/raytracer/Main.java
@@ -1,12 +1,10 @@
 package raytracer;
 
-import java.io.IOException;
-
 public class Main {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 800;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Window.init(WIDTH, HEIGHT);
 
         RayTracer rt = new RayTracer(WIDTH, HEIGHT);
